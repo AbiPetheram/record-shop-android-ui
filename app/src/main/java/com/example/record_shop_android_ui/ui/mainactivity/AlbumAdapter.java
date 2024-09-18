c96c5e3beb81cf5b8b381a3b5ccf3ee43587df30
AbiPetheram <abi.gill97@gmail.com>
2024-09-18 15:00:33 +0100

    feat: implement required methods from RecyclerView.Adapter in AlbumAdapter

--- app/src/main/java/com/example/record_shop_android_ui/ui/mainactivity/AlbumAdapter.java
+++ app/src/main/java/com/example/record_shop_android_ui/ui/mainactivity/AlbumAdapter.java
@@ -1,13 +1,49 @@
 package com.example.record_shop_android_ui.ui.mainactivity;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.record_shop_android_ui.R;
 import com.example.record_shop_android_ui.databinding.AlbumLayoutBinding;
 import com.example.record_shop_android_ui.model.Album;
 
+import java.util.List;
+
 public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder> {
+    List<Album> albumList;
+    Context context;
+
+    public AlbumAdapter(Context context, List<Album> albumList) {
+        this.context = context;
+        this.albumList = albumList;
+    }
+
+    @NonNull
+    @Override
+    public AlbumViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        AlbumLayoutBinding albumLayoutBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.album_layout,
+                parent,
+                false);
+        return new AlbumViewHolder(albumLayoutBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AlbumViewHolder holder, int position) {
+        holder.albumLayoutBinding.setAlbum(albumList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return albumList.size();
+    }
 
     public static class AlbumViewHolder extends RecyclerView.ViewHolder{
         AlbumLayoutBinding albumLayoutBinding;
