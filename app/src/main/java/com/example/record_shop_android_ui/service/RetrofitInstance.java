ed560f0b108fd24711d83fb63a71d715f2095928
AbiPetheram <abi.gill97@gmail.com>
2024-09-18 13:52:55 +0100

    fix: add http:// to the BASE_URL

--- app/src/main/java/com/example/record_shop_android_ui/service/RetrofitInstance.java
+++ app/src/main/java/com/example/record_shop_android_ui/service/RetrofitInstance.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
     private static Retrofit retrofit = null;
-    private static final String BASE_URL = "Record-shop-env.eba-pfu3zq8z.eu-west-2.elasticbeanstalk.com/";
+    private static final String BASE_URL = "http://Record-shop-env.eba-pfu3zq8z.eu-west-2.elasticbeanstalk.com/";
 
     public static AlbumApiService getService(){
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
