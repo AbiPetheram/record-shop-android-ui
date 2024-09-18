10c32d3f55e2e27fcd3dadb21a51c5298bed4167
AbiPetheram <abi.gill97@gmail.com>
2024-09-18 14:37:59 +0100

    feat: implement MainActivityViewModel class with constructor and getAllAlbums method

---
+++ app/src/main/java/com/example/record_shop_android_ui/ui/mainactivity/MainActivityViewModel.java
@@ -0,0 +1,25 @@
+package com.example.record_shop_android_ui.ui.mainactivity;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.record_shop_android_ui.model.Album;
+import com.example.record_shop_android_ui.service.AlbumRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    AlbumRepository albumRepository;
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.albumRepository = new AlbumRepository(application);
+    }
+
+    public LiveData<List<Album>> getAllAlbums(){
+        return albumRepository.getMutableLiveData();
+    }
+}
