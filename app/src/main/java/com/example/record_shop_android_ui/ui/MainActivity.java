773472da509053fee08a8ca1b08934cba91929d1
AbiPetheram <abi.gill97@gmail.com>
2024-09-18 14:37:29 +0100

    refactor: move MainActivity into ui package

--- app/src/main/java/com/example/record_shop_android_ui/ui/MainActivity.java
+++ app/src/main/java/com/example/record_shop_android_ui/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.record_shop_android_ui;
+package com.example.record_shop_android_ui.ui;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.record_shop_android_ui.R;
 
 public class MainActivity extends AppCompatActivity {
 
