06a2618161283ca4419a14bca3195ede4c02c49b
AbiPetheram <abi.gill97@gmail.com>
2024-09-18 12:13:53 +0100

    feat: implement getService() method in RetrofitInstance with logging

--- app/src/main/java/com/example/record_shop_android_ui/service/RetrofitInstance.java
+++ app/src/main/java/com/example/record_shop_android_ui/service/RetrofitInstance.java
@@ -1,4 +1,28 @@
 package com.example.record_shop_android_ui.service;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitInstance {
+    private static Retrofit retrofit = null;
+    private static final String BASE_URL = "Record-shop-env.eba-pfu3zq8z.eu-west-2.elasticbeanstalk.com/";
+
+    public static AlbumApiService getService(){
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+        return retrofit.create(AlbumApiService.class);
+    }
 }
