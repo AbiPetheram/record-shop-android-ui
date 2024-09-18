ec981406472b7482a36896a535d99c25e62281e5
AbiPetheram <abi.gill97@gmail.com>
2024-09-18 14:50:24 +0100

    feat: add Bindable annotation to getters and notifyPropertyChanged to setters in Album

--- app/src/main/java/com/example/record_shop_android_ui/model/Album.java
+++ app/src/main/java/com/example/record_shop_android_ui/model/Album.java
@@ -1,6 +1,9 @@
 package com.example.record_shop_android_ui.model;
 
 import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.example.record_shop_android_ui.BR;
 
 public class Album extends BaseObservable {
     private long id;
@@ -22,51 +25,63 @@ public class Album extends BaseObservable {
     public Album() {
     }
 
+    @Bindable
     public long getId() {
         return id;
     }
 
+    @Bindable
     public String getAlbumName() {
         return albumName;
     }
 
+    @Bindable
     public String getArtistName() {
         return artistName;
     }
 
+    @Bindable
     public long getReleaseYear() {
         return releaseYear;
     }
 
+    @Bindable
     public String getGenre() {
         return genre;
     }
 
+    @Bindable
     public long getStock() {
         return stock;
     }
 
     public void setId(long id) {
         this.id = id;
+        notifyPropertyChanged(BR.id);
     }
 
     public void setAlbumName(String albumName) {
         this.albumName = albumName;
+        notifyPropertyChanged(BR.id);
     }
 
     public void setArtistName(String artistName) {
         this.artistName = artistName;
+        notifyPropertyChanged(BR.id);
     }
 
     public void setReleaseYear(long releaseYear) {
         this.releaseYear = releaseYear;
+        notifyPropertyChanged(BR.id);
     }
 
     public void setGenre(String genre) {
         this.genre = genre;
+        notifyPropertyChanged(BR.id);
     }
 
     public void setStock(long stock) {
         this.stock = stock;
+        notifyPropertyChanged(BR.id);
     }
 }
