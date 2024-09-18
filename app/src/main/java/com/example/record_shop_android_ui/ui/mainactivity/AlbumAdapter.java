e2b104b82043266a408b2a18dc44759953185394
AbiPetheram <abi.gill97@gmail.com>
2024-09-18 14:55:37 +0100

    feat: implement AlbumViewHolder class in AlbumAdapter

--- app/src/main/java/com/example/record_shop_android_ui/ui/mainactivity/AlbumAdapter.java
+++ app/src/main/java/com/example/record_shop_android_ui/ui/mainactivity/AlbumAdapter.java
@@ -1,4 +1,21 @@
 package com.example.record_shop_android_ui.ui.mainactivity;
 
-public class AlbumAdapter {
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.record_shop_android_ui.databinding.AlbumLayoutBinding;
+import com.example.record_shop_android_ui.model.Album;
+
+public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder> {
+
+    public static class AlbumViewHolder extends RecyclerView.ViewHolder{
+        AlbumLayoutBinding albumLayoutBinding;
+
+        public AlbumViewHolder(AlbumLayoutBinding albumLayoutBinding) {
+            super(albumLayoutBinding.getRoot());
+            this.albumLayoutBinding = albumLayoutBinding;
+        }
+    }
+
 }
