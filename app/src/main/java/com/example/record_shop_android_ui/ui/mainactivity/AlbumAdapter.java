3bb6175efc8b9d3d153510c5596af0fcdb79e0c1
AbiPetheram <abi.gill97@gmail.com>
2024-09-19 11:05:23 +0100

    refactor: remove unused import in AlbumAdapter

--- app/src/main/java/com/example/record_shop_android_ui/ui/mainactivity/AlbumAdapter.java
+++ app/src/main/java/com/example/record_shop_android_ui/ui/mainactivity/AlbumAdapter.java
@@ -2,7 +2,6 @@ package com.example.record_shop_android_ui.ui.mainactivity;
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
