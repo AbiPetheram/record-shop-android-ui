e184ca88d8173ed6c66a0b51e24c88ff91ca9bde
AbiPetheram <abi.gill97@gmail.com>
2024-09-19 10:38:21 +0100

    feat: add post method addAlbum in AlbumApiService

--- app/src/main/java/com/example/record_shop_android_ui/service/AlbumApiService.java
+++ app/src/main/java/com/example/record_shop_android_ui/service/AlbumApiService.java
@@ -5,9 +5,14 @@ import com.example.record_shop_android_ui.model.Album;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface AlbumApiService {
     @GET("api/v1/album")
     Call<List<Album>> getAllAlbums();
+
+    @POST("api/v1/album")
+    Call<Album> addAlbum(@Body Album album);
 }
