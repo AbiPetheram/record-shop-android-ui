63c9aa1d8b4273501528787efc2464ab60a5d9af
AbiPetheram <abi.gill97@gmail.com>
2024-09-19 17:12:16 +0100

    feat: implement MainActivityClickHandler to handle clicking the add new album button

--- app/src/main/java/com/example/record_shop_android_ui/ui/mainactivity/MainActivityClickHandler.java
+++ app/src/main/java/com/example/record_shop_android_ui/ui/mainactivity/MainActivityClickHandler.java
@@ -1,4 +1,20 @@
 package com.example.record_shop_android_ui.ui.mainactivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+import com.example.record_shop_android_ui.ui.addalbum.AddNewAlbumActivity;
+
 public class MainActivityClickHandler {
+    Context context;
+
+    public MainActivityClickHandler(Context context) {
+        this.context = context;
+    }
+
+    public void onClickAddAlbum(View view){
+        Intent intent = new Intent(context, AddNewAlbumActivity.class);
+        context.startActivity(intent);
+    }
 }
