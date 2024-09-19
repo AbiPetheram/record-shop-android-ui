82dfcd64ba62c1b04aa88cdfff8e5eb6afa69ecd
AbiPetheram <abi.gill97@gmail.com>
2024-09-19 15:04:23 +0100

    feat: implement data binding and click handlers in AddNewAlbumActivity

--- app/src/main/java/com/example/record_shop_android_ui/ui/addalbum/AddNewAlbumActivity.java
+++ app/src/main/java/com/example/record_shop_android_ui/ui/addalbum/AddNewAlbumActivity.java
@@ -7,10 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.record_shop_android_ui.R;
+import com.example.record_shop_android_ui.databinding.ActivityAddNewAlbumBinding;
+import com.example.record_shop_android_ui.model.Album;
+import com.example.record_shop_android_ui.ui.mainactivity.MainActivityViewModel;
 
 public class AddNewAlbumActivity extends AppCompatActivity {
+    private Album album;
+    private AddAlbumClickHandlers clickHandlers;
+    private ActivityAddNewAlbumBinding binding;
+    private MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +31,12 @@ public class AddNewAlbumActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        album = new Album();
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_new_album);
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        clickHandlers = new AddAlbumClickHandlers(album, this, viewModel);
+        binding.setAlbum(album);
+        binding.setClickHandler(clickHandlers);
+
     }
 }
