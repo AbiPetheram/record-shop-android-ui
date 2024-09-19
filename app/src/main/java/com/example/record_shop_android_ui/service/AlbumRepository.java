21425e1abbb38f8a7229b245e7e36464edea9725
AbiPetheram <abi.gill97@gmail.com>
2024-09-19 10:51:04 +0100

    feat: implement onResponse and onFailure methods using Toast

--- app/src/main/java/com/example/record_shop_android_ui/service/AlbumRepository.java
+++ app/src/main/java/com/example/record_shop_android_ui/service/AlbumRepository.java
@@ -2,6 +2,7 @@ package com.example.record_shop_android_ui.service;
 
 import android.app.Application;
 import android.provider.MediaStore;
+import android.widget.Toast;
 
 import androidx.lifecycle.MutableLiveData;
 
@@ -45,12 +46,16 @@ public class AlbumRepository {
         call.enqueue(new Callback<Album>() {
             @Override
             public void onResponse(Call<Album> call, Response<Album> response) {
-
+                Toast.makeText(application.getApplicationContext(),
+                        response.body().getAlbumName() + " added to database",
+                        Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onFailure(Call<Album> call, Throwable t) {
-
+                Toast.makeText(application.getApplicationContext(),
+                        "Unable to add album to database",
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
