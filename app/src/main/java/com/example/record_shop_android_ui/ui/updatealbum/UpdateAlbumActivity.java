4729d8886fc2e9e9ccd8107ca840d522ad2d32de
AbiPetheram <abi.gill97@gmail.com>
2024-09-20 14:57:26 +0100

    feat: implement UpdateAlbumActivity onCreate functionality to set binding and click handler

--- app/src/main/java/com/example/record_shop_android_ui/ui/updatealbum/UpdateAlbumActivity.java
+++ app/src/main/java/com/example/record_shop_android_ui/ui/updatealbum/UpdateAlbumActivity.java
@@ -4,23 +4,32 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.record_shop_android_ui.R;
+import com.example.record_shop_android_ui.databinding.ActivityUpdateAlbumBinding;
+import com.example.record_shop_android_ui.model.Album;
+import com.example.record_shop_android_ui.ui.mainactivity.MainActivityViewModel;
 
 public class UpdateAlbumActivity extends AppCompatActivity {
+    private Album album;
+    private ActivityUpdateAlbumBinding binding;
+    private UpdateAlbumClickHandler clickHandler;
+    private final String ALBUM_KEY = "album";
+    private MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_update_album);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        album = getIntent().getParcelableExtra(ALBUM_KEY);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_update_album);
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        clickHandler = new UpdateAlbumClickHandler(album, viewModel, this);
+        binding.setAlbum(album);
+        binding.setClickHandler(clickHandler);
     }
 }
