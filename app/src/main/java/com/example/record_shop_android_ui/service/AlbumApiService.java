e73b36a8f374224ec30e44cd21125571431811a8
AbiPetheram <abi.gill97@gmail.com>
2024-09-20 11:21:15 +0100

    feat: add PUT and DELETE methods in AlbumApiService

--- app/src/main/java/com/example/record_shop_android_ui/service/AlbumApiService.java
+++ app/src/main/java/com/example/record_shop_android_ui/service/AlbumApiService.java
@@ -6,8 +6,11 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface AlbumApiService {
     @GET("api/v1/album")
@@ -15,4 +18,10 @@ public interface AlbumApiService {
 
     @POST("api/v1/album")
     Call<Album> addAlbum(@Body Album album);
+
+    @PUT("api/v1/album/{id}")
+    Call<Album> updateAlbum(@Path("id") Long id, @Body Album album);
+
+    @DELETE("api/v1/album/{id}")
+    Call<String> deleteAlbum(@Path("id") Long id);
 }
